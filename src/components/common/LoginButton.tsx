08a5e7c23437cebb2943dd26716e21cef0c9e654
YooJung Kwon <best106yj@naver.com>
2025-04-30 16:13:38 +0900

    🎨 로그인 버튼 공통 컴포넌트 생성

---
+++ src/components/common/LoginButton.tsx
@@ -0,0 +1,9 @@
+export default function LoginButton() {
+  return (
+    <>
+      <div className="w-[327px] h-[54px] bg-[#8EF3BF] rounded-[10px] flex items-center justify-center">
+        <span className="text-[18px] font-bold">Login</span>
+      </div>
+    </>
+  );
+}
