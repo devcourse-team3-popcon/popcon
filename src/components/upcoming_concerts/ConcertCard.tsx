3fe3408a00148c1b9c77cb74bc281ab83f527df8
mjlee <leeminji9938@naver.com>
2025-05-02 20:38:16 +0900

    🎨 Concert Card 컴포넌트 스타일링

---
+++ src/components/upcoming_concerts/ConcertCard.tsx
@@ -0,0 +1,11 @@
+export default function ConcertCard() {
+  return (
+    <a href={""} target='_blank' className='w-[320px] h-[580px] border '>
+      <img src='{img}' className='w-full h-[400px]' />
+      <h2>title</h2>
+      <h3>artist</h3>
+      <p>date</p>
+      <p>in place</p>
+    </a>
+  );
+}
