754790f7ed865caeec184c250ebc745e125cda7d
hxezu <hailey_apple@icloud.com>
2025-05-06 00:24:59 +0900

    ✨ 게시물 상세 페이지 스타일링 ( 본문, 댓글 공통 컴포넌트 생성)

--- src/features/community/components/PostDetail.tsx
+++ src/features/community/components/PostDetail.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import Article from "../../../components/common/Article";
+import Comment from "../../../components/common/Comment";
+import TextAreaField from "../../../components/common/TextAreaField";
+
 export default function PostDetail() {
+  const [comment, setComment] = useState("");
   return (
     <>
-      <h1 className="bg-amber-400">PostDetail Component</h1>
+      <div className="flex flex-col gap-8">
+        <Article />
+        <Comment />
+        <TextAreaField
+          label="댓글 작성"
+          id="contentInput"
+          name="contentInput"
+          autoComplete="contentInput"
+          placeholder="댓글을 입력해주세요."
+          className="flex-grow w-full h-[150px]"
+          value={comment}
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+        />
+      </div>
+      <div className="w-full flex justify-end">
+        <button className="cursor-pointer text-[14px] px-6 py-2 bg-(--primary-300)  text-(--bg-color) w-fit rounded-4xl font-semibold  mt-2">
+          작성 완료
+        </button>
+      </div>
     </>
   );
 }
