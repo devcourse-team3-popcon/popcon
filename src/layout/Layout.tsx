003dfaf3b8b16596da962d4e80a9d5c701977083
Jungyu Park <parkjungyu09@gmail.com>
2025-05-06 22:37:42 +0900

    🎨 Back 버튼 컴포넌트 구현

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router";
 import Header from "./header/Header";
 
 export default function Layout() {
   return (
     <div className="py-4 px-[104px]">
       <Header />
+      <Outlet />
     </div>
   );
 }
