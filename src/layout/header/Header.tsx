25408544c2166d506c363f4d5ed4b523e1724eec
Jungyu Park <parkjungyu09@gmail.com>
2025-05-12 12:07:28 +0900

    🎨 Sidebar Menu 컴포넌트 생성

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,6 @@
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import SideMenu from "./SideMenu";
 import UserSection from "./UserSection";
 
 export default function Header() {
@@ -8,6 +9,7 @@ export default function Header() {
       <div className="md:hidden">
         <div>
           <Logo />
+          <SideMenu />
         </div>
       </div>
 
