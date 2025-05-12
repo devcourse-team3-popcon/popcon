cd811d01dc7e4bce7d90735b7d99e2f3de171c27
Jungyu Park <parkjungyu09@gmail.com>
2025-05-12 12:28:29 +0900

    🎨 About us 루트 컨테이너 패딩 바텀 추가

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -3,7 +3,7 @@ import AboutUsContent from "../components/aboutus/AboutUsContent";
 
 export default function AboutUs() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center pb-20">
       <AboutUsBanner />
       <AboutUsContent />
     </div>
