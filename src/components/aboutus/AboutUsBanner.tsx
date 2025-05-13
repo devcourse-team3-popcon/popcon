d380cf5fa5ebe5acfc83e1651c308bfbd66f2c49
Jungyu Park <parkjungyu09@gmail.com>
2025-05-13 14:27:32 +0900

    🎨 About Us 반응형 적용

--- src/components/aboutus/AboutUsBanner.tsx
+++ src/components/aboutus/AboutUsBanner.tsx
@@ -1,9 +1,19 @@
 export default function AboutUsBanner() {
   return (
-    <div className="flex justify-center items-center w-full px-16  py-12">
-      <p className="font-[MonumentExtended] text-start text-[55px] text-[color:var(--white)]">
-        ABOUT US
-      </p>
-    </div>
+    <>
+      <div className="hidden md:flex justify-center items-center w-full px-16  py-12">
+        <p className="font-[MonumentExtended] text-start text-[55px] text-[color:var(--white)]">
+          ABOUT US
+        </p>
+      </div>
+      <div className="flex-col justify-center items-center w-full px-16  py-12 md:hidden">
+        <p className="font-[MonumentExtended] text-start text-[55px] text-[color:var(--white)]">
+          ABOUT
+        </p>
+        <p className="font-[MonumentExtended] text-center text-[55px] text-[color:var(--white)]">
+          US
+        </p>
+      </div>
+    </>
   );
 }
