9ba532d8e5767187cf52c94da83e55d0b65e1d19
hxezu <hailey_apple@icloud.com>
2025-05-14 21:21:30 +0900

    💄 문구 추가

--- src/features/home/components/Section1.tsx
+++ src/features/home/components/Section1.tsx
@@ -6,14 +6,14 @@ export default function Section1() {
           <div className="flex flex-col gap-8 w-full max-w-2xl xl:-translate-y-10 pr-2">
             <div className="flex flex-col gap-2 w-full ">
               <h1 className="font-[MonumentExtended] w-full text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl 2xl:text-8xl text-[color:var(--primary-300)]">
-                POPcon
+                POP<span className="text-[color:var(--white)]">con</span>.
               </h1>
               <h3 className="font-light text-base sm:text-lg lg:text-xl xl:text-xl 2xl:text-2xl ">
                 해외 팝송 팬들을 위한 커뮤니티 기반 음악 플랫폼 : 팝콘
               </h3>
             </div>
             <span className="text-xs sm:text-sm text-[color:var(--white-80)]">
-              @2025
+              @2025 DEVCOURSE
             </span>
           </div>
         </div>
