80ccb4a78d87ca0e454c7d44a487afaa97648b26
hxezu <hailey_apple@icloud.com>
2025-05-14 21:14:18 +0900

    💄 Home Section1 반응형 스타일링 추가

--- src/features/home/components/Section1.tsx
+++ src/features/home/components/Section1.tsx
@@ -1,35 +1,27 @@
 export default function Section1() {
   return (
     <>
-      <section className="h-[95vh] justify-center flex items-center overflow-hidden">
-        <div className="flex-1 h-full relative w-full ">
-          <div className="pl-40 items-baseline h-auto bottom-40 absolute flex flex-col gap-8">
-            <div className="flex flex-col gap-2">
-              <div className="relative inline-block w-fit">
-                <img
-                  src="/src/assets/images/popcon.svg"
-                  alt="팝콘 로고"
-                  className="absolute -top-10 -right-18 w-10 h-10 sm:w-12 sm:h-12 scale-150"
-                />
-                <h1 className="font-[MonumentExtended] text-4xl sm:text-[56px] lg:text-[72px] text-[color:var(--primary-300)] ">
-                  POPcon
-                </h1>
-              </div>
-              <h3 className="font-light text-base sm:text-lg lg:text-2xl ">
+      <section className="h-auto min-h-[95vh] justify-center items-start flex flex-col xl:flex-row xl:items-end overflow-hidden pl-6 sm:pl-12 lg:pl-24 xl:pl-40 w-full">
+        <div className="md:basis-[35%] flex xl:items-baseline-last justify-center h-full py-20 xl:pb-20">
+          <div className="flex flex-col gap-8 w-full max-w-2xl xl:-translate-y-10 pr-2">
+            <div className="flex flex-col gap-2 w-full ">
+              <h1 className="font-[MonumentExtended] w-full text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl 2xl:text-8xl text-[color:var(--primary-300)]">
+                POPcon
+              </h1>
+              <h3 className="font-light text-base sm:text-lg lg:text-xl xl:text-xl 2xl:text-2xl ">
                 해외 팝송 팬들을 위한 커뮤니티 기반 음악 플랫폼 : 팝콘
               </h3>
             </div>
-
             <span className="text-xs sm:text-sm text-[color:var(--white-80)]">
               @2025
             </span>
           </div>
         </div>
-        <div className="flex-1 relative w-full flex justify-center">
+        <div className="w-full md:basis-[65%] flex justify-end items-end h-full xl:translate-y-20">
           <img
             src="/src/assets/images/section1.svg"
             alt="목업 이미지"
-            className=" w-[80%] sm:w-[70%] lg:w-[90%] -top-70 absolute scale-110 object-contain"
+            className="max-h-[75%] object-contain"
           />
         </div>
       </section>
