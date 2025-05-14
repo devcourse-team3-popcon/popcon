2c4d5487f5ac84390d919447184bf728707a1a48
YooJung Kwon <best106yj@naver.com>
2025-05-14 21:16:37 +0900

    💄 대화목록 반응형 디자인 수정

--- src/features/chat/components/ChatUser.tsx
+++ src/features/chat/components/ChatUser.tsx
@@ -45,7 +45,7 @@ export default function ChatUser({
           isSelected && "bg-[var(--grey-500)]"
         }`}
       >
-        <div>
+        <div className="lg:block hidden">
           <div className="relative">
             <img
               src={userImage ? userImage : defaultProfile}
@@ -63,7 +63,16 @@ export default function ChatUser({
         </div>
 
         <div className="flex flex-col flex-1 p-0.5 w-[50%]">
-          <div className="text-base font-medium">{userName}</div>
+          <div className="text-base font-medium flex gap-2 items-center">
+            {userName}
+            {userIsOnline && (
+              <img
+                src={onlineIcon}
+                alt="온라인 표시"
+                className="lg:hidden block size-2"
+              />
+            )}
+          </div>
 
           <div className="flex items-center justify-between gap-2">
             <div className="text-[0.75rem] font-regular text-[var(--white-80)] truncate flex-1">
