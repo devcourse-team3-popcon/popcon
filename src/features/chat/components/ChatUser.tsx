cc074ce55d1381c839896f8f2f88b13190091fd4
YooJung Kwon <best106yj@naver.com>
2025-05-16 01:41:06 +0900

    💄 css 추가 수정

--- src/features/chat/components/ChatUser.tsx
+++ src/features/chat/components/ChatUser.tsx
@@ -58,7 +58,7 @@ export default function ChatUser({
             <img
               src={onlineIcon}
               alt="온라인 표시"
-              className="absolute right-0.5 bottom-0.5"
+              className="absolute left-6 bottom-0"
             />
           )}
         </div>
