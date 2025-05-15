d4c115fe9b248bb080f375ba9502a5c7e1503621
Jungyu Park <parkjungyu09@gmail.com>
2025-05-15 16:36:39 +0900

    🚀 Feat: PlaylistTrackItem 앨범 아닌 list 클릭시 노래 재생으로 변경

--- src/features/playlist/components/PlaylistPanel/PlaylistTrackItem.tsx
+++ src/features/playlist/components/PlaylistPanel/PlaylistTrackItem.tsx
@@ -1,9 +1,9 @@
-import {ListMusic, Pause, Play, Trash2} from "lucide-react";
-import {usePlaylistStore} from "../../../../stores/playlistStore";
-import {deleteTrackFromPlaylist} from "../../../../apis/playlist/playlistService";
-import {useAddTrackToPlaylist} from "../../hooks/useAddTrackToPlaylist";
-import {useState} from "react";
-import {searchYoutubeVideo} from "../../../../apis/youtube/youtubeSearch";
+import { ListMusic, Pause, Play, Trash2 } from "lucide-react";
+import { usePlaylistStore } from "../../../../stores/playlistStore";
+import { deleteTrackFromPlaylist } from "../../../../apis/playlist/playlistService";
+import { useAddTrackToPlaylist } from "../../hooks/useAddTrackToPlaylist";
+import { useState } from "react";
+import { searchYoutubeVideo } from "../../../../apis/youtube/youtubeSearch";
 
 export default function PlaylistTrackItem({
   track,
@@ -15,12 +15,15 @@ export default function PlaylistTrackItem({
   currentVideo,
   setCurrentVideo,
 }: PlaylistTrackItemProps) {
-  const {setTracks} = usePlaylistStore();
+  const { setTracks } = usePlaylistStore();
   const tracks = usePlaylistStore((state) => state.tracks);
   const handleTrackClick = useAddTrackToPlaylist();
   const [videoId, setVideoId] = useState<string | null>(null);
 
-  const deleteTrack = async (e: React.MouseEvent<SVGSVGElement>, trackId: string) => {
+  const deleteTrack = async (
+    e: React.MouseEvent<SVGSVGElement>,
+    trackId: string
+  ) => {
     e.stopPropagation();
     await deleteTrackFromPlaylist(trackId);
     const updatedTracks = tracks.filter((track) => track._id !== trackId);
@@ -50,9 +53,11 @@ export default function PlaylistTrackItem({
   let isPlaying = false;
 
   if (item) {
-    isPlaying = currentVideo?.postId === item?._id && currentVideo?.videoId === videoId;
+    isPlaying =
+      currentVideo?.postId === item?._id && currentVideo?.videoId === videoId;
   } else if (track) {
-    isPlaying = currentVideo?.postId === track.id && currentVideo?.videoId === videoId;
+    isPlaying =
+      currentVideo?.postId === track.id && currentVideo?.videoId === videoId;
   }
 
   const togglePlayTrack = async () => {
@@ -69,7 +74,9 @@ export default function PlaylistTrackItem({
       query = `${item.title.artist} - ${item.title.name} official audio topic`;
       postId = item._id;
     } else if (track) {
-      query = `${track.artists[0]?.name || track.artists} - ${track.name} official audio topic`;
+      query = `${track.artists[0]?.name || track.artists} - ${
+        track.name
+      } official audio topic`;
       postId = track.id;
     }
 
@@ -85,13 +92,15 @@ export default function PlaylistTrackItem({
 
   return (
     <div className="flex h-auto p-2 md:p-[18px] justify-between items-center hover:bg-[color:var(--grey-500)] rounded-[10px] group cursor-pointer">
-      <div className="flex gap-[24px] items-center flex-1 overflow-hidden">
+      <div
+        className="flex gap-[24px] items-center flex-1 overflow-hidden"
+        onClick={togglePlayTrack}
+      >
         <div className="flex rounded-[10px] justify-center items-center relative group overflow-hidden">
           <img
             src={imageUrl}
             alt={imageAlt}
             className="w-10 h-10 md:w-15 md:h-15 flex-shrink-0"
-            onClick={togglePlayTrack}
           />
           <div
             className={`absolute flex justify-center items-center opacity-0 group-hover:opacity-100 group-hover:bg-black/50 w-full h-full z-10 ${
@@ -99,16 +108,16 @@ export default function PlaylistTrackItem({
             }`}
           >
             {isPlaying ? (
-              <Pause onClick={togglePlayTrack} className='z-50' />
+              <Pause onClick={togglePlayTrack} className="z-50" />
             ) : (
-              <Play onClick={togglePlayTrack} className='z-50' />
+              <Play onClick={togglePlayTrack} className="z-50" />
             )}
           </div>
           {isPlaying && currentVideo?.videoId && (
             <iframe
-              className='w-0 h-0 hidden'
+              className="w-0 h-0 hidden"
               src={`https://www.youtube.com/embed/${currentVideo.videoId}?autoplay=1&mute=0&controls=0&modestbranding=1&rel=0&showinfo=0`}
-              allow='autoplay'
+              allow="autoplay"
               allowFullScreen
             />
           )}
@@ -126,16 +135,16 @@ export default function PlaylistTrackItem({
         </div>
       </div>
       {showEllipsis && (
-        <div className='ml-2 flex-shrink-0'>
+        <div className="ml-2 flex-shrink-0">
           <Trash2
-            className='cursor-pointer invisible group-hover:visible transition-all'
+            className="cursor-pointer invisible group-hover:visible transition-all"
             onClick={(e) => deleteTrack(e, trackId)}
           />
         </div>
       )}
       {other && (
-        <div className='ml-2 flex-shrink-0' onClick={handleListMusicClick}>
-          <ListMusic className='cursor-pointer invisible group-hover:visible transition-all' />
+        <div className="ml-2 flex-shrink-0" onClick={handleListMusicClick}>
+          <ListMusic className="cursor-pointer invisible group-hover:visible transition-all" />
         </div>
       )}
     </div>
