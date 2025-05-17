122a2772b73061adee6cb4c525e65e20c0c206b2
mjlee <leeminji9938@naver.com>
2025-05-17 14:24:57 +0900

    💄 Upcoming Concerts 스켈레톤 ui 수정

--- src/features/upcoming_concerts/components/ConcertListSkeleton.tsx
+++ src/features/upcoming_concerts/components/ConcertListSkeleton.tsx
@@ -1,9 +1,11 @@
 import ConcertCardSkeleton from "./ConcertCardSkeleton";
 
 export default function ConcertListSkeleton() {
+  const dummyArray = Array.from({length: 12});
+
   return (
-    <div className='flex flex-wrap justify-center gap-x-10 gap-y-10 w-full md:gap-x-20 md:gap-y-20'>
-      {Array.from({length: 8}).map((_, index) => (
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-8 md:gap-y-20 gap-x-10 w-full mx-auto place-items-center'>
+      {dummyArray.map((_, index) => (
         <ConcertCardSkeleton key={index} />
       ))}
     </div>
